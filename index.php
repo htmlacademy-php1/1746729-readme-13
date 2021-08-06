167a77c2ee662acd860ca1b14ad01b10502dfd4d
ваше имя <87047363+Yerkebulan1981@users.noreply.github.com>
2021-08-06 20:13:56 +0600

    дополнил свое имя в условие

--- index.php
+++ index.php
@@ -42,9 +42,10 @@ $user_name = 'Yerkebulan Ardabayev'; // укажите здесь ваше им�
         <div class="header__nav-wrapper">
             <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
             <nav class="header__nav">
-                <?php if ($is_auth==1):>
-                <p>Yerkebulan Ardabayev</p>
-                <ul class="header__my-nav">
+                <?php if ($is_auth==1):?>
+                    <p>Yerkebulan Ardabayev</p>
+                <?php endif; ?>
+                               <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
                         <a class="header__page-link header__page-link--active" title="Популярный контент">
                             <span class="visually-hidden">Популярный контент</span>
